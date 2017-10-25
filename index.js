0d12a7ad2857d71dce7a7c539775ac0801d42dc8
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-25 22:46:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much."
+
+
+var kittens = ["Milo", "Otis", "Garfield"];
+console.log(kittens.length);
+
+function destrutivelyRemoveLastKitten(){
+   kittens.pop();
+   return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift(0);
+  return array;
+}
